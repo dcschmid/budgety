df093662eb82d1716aafe376ac535227326b3bb3
Daniel Schmid <daniel.schmid@libertymail.net>
2019-06-21 23:40:51 +0200

    Updating the percentages: UI Controller

--- app.js
+++ app.js
@@ -150,6 +150,7 @@ var UIController = (function() {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
+    expensesPercLabel: '.item__percentage',
   };
 
   return {
@@ -214,6 +215,24 @@ var UIController = (function() {
 
     },
 
+      displayPercentages: function(percentages) {
+        var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+          var nodeListForEach = function(list, callback) {
+            for (var i = 0, l = list.length; i < l; i++) {
+                callback(list[i], i);
+            }
+          };
+
+          nodeListForEach(fields, function(current, index) {
+              if (percentages[index] > 0) {
+                current.textContent = percentages[index] + '%';
+              } else {
+                current.textContent = '---';
+              }
+          });
+      },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -259,7 +278,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
   var ctrlAddItem = function() {
